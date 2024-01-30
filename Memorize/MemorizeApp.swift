4884d7bcae4b889b99ca8b2e52bba41bdf98a418
Markus Douglas <makusdouglas@gmail.com>
2024-01-29 23:19:23 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Markus Douglas on 29/01/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
