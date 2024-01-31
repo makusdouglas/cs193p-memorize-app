a7ecdb83765658a1819371a78d8651178d8235e9
Markus Douglas <makusdouglas@gmail.com>
2024-01-30 23:53:35 -0300

    chore: - create some components to organize the code; - create the logic to flip the cards - Adding a ScrollView around the cards - Adding a LazyVGrid around the cards

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,35 +8,97 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var cardCount: Int = 8
+    var emojis: Array<String> = ["💀", "⚽️", "🍎", "👻", "🎹", "🚓", "🎲", "❤️", "😈", "🐶", "🐸", "🔥"]
+    
     var body: some View {
-        HStack {
-            CardView(text: "💀", isFaceUp: true)
-            CardView(text: "🍎")
-            CardView(text: "⚽️")
-            CardView(text: "👻")
+        VStack{
+            ScrollView {
+                Cards
+            }
+            Spacer(minLength: 24)
+            CardCountAdjusters
+            
         }
         .padding()
-        .foregroundColor(.orange)
+    }
+    
+    var Cards: some View {
+        LazyVGrid (columns: [GridItem(), GridItem(), GridItem(), GridItem()]) {
+            ForEach(0..<cardCount, id: \.self) {index in
+                if emojis.indices.contains(index) {
+                    CardView(content: emojis[index])
+                        .aspectRatio(2/3, contentMode: ContentMode.fit)
+                }
+            }
+        }
+        .foregroundColor(.teal)
+    }
+    
+    var CardCountAdjusters: some View {
+        HStack {
+            CardRemover
+            Spacer()
+            Text("000 points").foregroundColor(.pink)
+            Spacer()
+            CardAdder
+        }
+    }
+    
+    func makeCardCountAdjuster(
+        by offset: Int,
+        symbol: String,
+        label: String,
+        alignment: HorizontalAlignment = HorizontalAlignment.leading
+    ) -> some View {
+        Button(
+            action: {
+                cardCount += offset
+            },
+            label: {
+                VStack (alignment: alignment) {
+                    Image(systemName: symbol)
+                    Text(label)
+                }
+            }
+        )
+        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+    }
+    
+    var CardRemover: some View {
+        makeCardCountAdjuster(
+            by: -1,
+            symbol: "minus.square",
+            label: "remove card"
+        )
+    }
+    
+    var CardAdder: some View {
+        makeCardCountAdjuster(
+            by: +1,
+            symbol: "plus.square",
+            label: "add card",
+            alignment: HorizontalAlignment.trailing
+        )
     }
 }
 
 
 struct CardView: View {
-    var text: String
+    let content: String
     @State var isFaceUp: Bool = false
     
     var body: some View {
         ZStack {
-            if isFaceUp {
-                RoundedRectangle(cornerRadius: 12)
-                    .foregroundColor(.white)
-                RoundedRectangle(cornerRadius: 12)
-                    .strokeBorder(lineWidth: 2)
-                Text(text)
-                    .font(.largeTitle)
-            } else {
-                RoundedRectangle(cornerRadius: 12)
+            let base = RoundedRectangle(cornerRadius: 12)
+            Group {
+                base.foregroundColor(.white)
+                base.strokeBorder(lineWidth: 2)
+                Text(content).font(.largeTitle)
             }
+            .opacity(isFaceUp ? 1 : 0)
+            base .opacity(isFaceUp ? 0 : 1)
+            
         }.onTapGesture {
             isFaceUp.toggle()
         }
